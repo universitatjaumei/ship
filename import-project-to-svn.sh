c292d969c903a7fb2fa2554a6cb8f6f687d14c8e
Ricardo Borillo <borillo@gmail.com>
2015-06-02 19:53:48 +0200

    Auxiliar script for importing content to SVN

---
+++ import-project-to-svn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+svn mkdir -m "Import code for project $1" svn://localhost/repos/$1
+svn mkdir -m "Import code for project $1" svn://localhost/repos/$1/tags
+svn mkdir -m "Import code for project $1" svn://localhost/repos/$1/branches
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $dir/code/$2
+svn import -m "Import code for project $1" . svn://localhost/repos/$1/trunk
