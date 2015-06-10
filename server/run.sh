2ba2758e34bf70a0368114cc528ed1e384c75876
Ricardo Borillo <borillo@gmail.com>
2015-06-10 13:40:16 +0200

    Remove and load containers by name

--- server/run.sh
+++ server/run.sh
@@ -1,12 +1,10 @@
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-
-docker run -p 8080:80 -v /home/borillo/Descargas:/usr/share/nginx/html:ro -d nginx
+docker rm -f ship-deploy-server
+docker rm -f ship-svn
 
 docker build -t ship/deploy-server .
 
-docker run -d -p 2222:22 -p 8888:8080 ship/deploy-server
-docker run -e SVN_REPONAME=repos -p 3690:3690 -d erikxiv/subversion
+docker run -d -p 2222:22 -p 8888:8080 --name ship-deploy-server ship/deploy-server
+docker run -e SVN_REPONAME=repos -p 3690:3690 -d --name ship-svn erikxiv/subversion
 
 sleep 1
 
